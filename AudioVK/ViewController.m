cf408fa7cd4e8d9aeaf5110f433c50e8c590a54d
Cybernik9 <yuriihuber@macminissu8.training.local>
2015-11-15 16:32:35 +0200

    Initial Commit

---
+++ AudioVK/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AudioVK
+//
+//  Created by Yurii Huber on 15.11.15.
+//  Copyright © 2015 Yurii Huber. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
